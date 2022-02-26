7d0f2512302de18376e24f5220e61a4ed0ea489b
Ojo Adeola Gabriel <ojoadeolagabriel.personal@gmail.com>
2022-02-26 06:06:14 +0000

    autogate-go-core: changes for v0.1.1645855573

--- _deployment/deploy.sh
+++ _deployment/deploy.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+# see: https://go.dev/doc/modules/publishing
+
 set -x
 
 MY_PATH=$(dirname "$0")
@@ -11,12 +13,11 @@ VERSION_PATCH="$(date +%s)"
 
 CURRENT_VERSION="v$VERSION_MAJOR.$VERSION_MINOR.$VERSION_PATCH"
 
-cd "$MY_PATH"
+cd "$MY_PATH" || return
 
 go mod tidy
 go test ./...
 git add .
 git commit -m "autogate-go-core: changes for $CURRENT_VERSION"
-git push
 git tag "$CURRENT_VERSION"
 git push origin "$CURRENT_VERSION"
